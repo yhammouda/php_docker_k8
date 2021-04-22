996be9bec7918859b8729bff39f6ce07eadc2142
yhammouda <joseph_hh_94@hotmail.com>
2021-04-22 16:30:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-<<<<<<< HEAD
-    echo 'My username is ' .$_ENV["USER"] . '!';
-=======
-echo 'My username is     test' .$_ENV["USER"] . '!';
->>>>>>> origin/main
+    echo 'My username is     test' .$_ENV["USER"] . '!';
 ?>
